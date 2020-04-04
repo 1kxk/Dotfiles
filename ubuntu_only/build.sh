93b8589d4805f8666327ddcb04615bd1fad10a99
1kxk <caiow.wk@gmail.com>
2020-04-04 16:04:08 -0300

    .

--- ubuntu_only/build.sh
+++ ubuntu_only/build.sh
@@ -8,3 +8,7 @@ mkdir -p ~/.icons && mkdir -p ~/.themes
 cp -r ~/flat-remix/Flat-Remix* ~/.icons/ && cp -r ~/flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
 
 
+# GNOME TERMINAL
+sudo apt-get install dconf-cli
+git clone https://github.com/dracula/gnome-terminal "$HOME/gnome-terminal"
+~/gnome-terminal/install.sh
