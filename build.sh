4bd5dc787a259f6e0fc625e56506c43d4645d09f
1kxk <caiow.wk@gmail.com>
2020-04-03 22:29:08 -0300

    scripts

--- build.sh
+++ build.sh
@@ -2,3 +2,27 @@
 # Build enviroment #
 ####################
 
+## SETUP MAIN PACKAGES
+ESSENTIAL_PACKAGES="git vim wget curl build-essential"
+
+echo -e "Do you want install your own packages or leave the default settings?\n(1) - Default (leave blank)\n(2) - My Packages"
+read -p "" CHOICE
+
+if [ $CHOICE -eq 1 ] || [ -z $CHOICE ]; then
+  CUSTOM_PACKAGES="$(cat .default-packages)"
+else
+  CUSTOM_PACKAGES="$(cat .custom-packages)"
+fi
+
+sudo apt update
+sudo apt install $ESSENTIAL_PACKAGES $CUSTOM_PACKAGES -y
+sudo apt upgrade -y
+
+## SETUP FONTS
+sudo apt install fonts-firacode -y
+
+## SETUP TERMINAL
+
+sudo apt install zsh -y
+export ZSH_CUSTOM=~/.oh-my-zsh/custom
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
---
+++ zsh_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###################
+# TERMINAL CONFIG #
+###################
+
+## SPACESHIP
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+## PLUGINS
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+
