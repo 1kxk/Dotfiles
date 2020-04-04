1eeb7a7e75c3d977933b110d6a4cb3fefcf13c87
1kxk <caiow.wk@gmail.com>
2020-04-04 18:22:26 -0300

    fixing bugs

--- stuffs-python.sh
+++ stuffs-python.sh
@@ -3,18 +3,17 @@
 ## Install Anaconda
 echo -e "DOWNLOADING anaconda....\n"
 wget -P "$HOME/Downloads/" https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh 
-zsh ~/Downloads/Anaconda3-2020.02-Linux-x86_64.sh
-
-zsh
+#zsh ~/Downloads/Anaconda3-2020.02-Linux-x86_64.sh
+#zsh
 
 ## Config Anaconda
-conda init zsh
-conda update -n base -c defaults conda
+#conda init zsh
+#conda update -n base -c defaults conda
 
-conda create --name web django flask
-conda create --name scripting pillow beautifulsoup4
-conda create --name data spyder numpy pandas matplotlib scipy seaborn scikit-learn
+#conda create --name web django flask
+#conda create --name scripting pillow beautifulsoup4
+#conda create --name data spyder numpy pandas matplotlib scipy seaborn scikit-learn
 
-python -m ipykernel install --user --name web
-python -m ipykernel install --user --name scripting
-python -m ipykernel install --user --name data
+#python -m ipykernel install --user --name web
+#python -m ipykernel install --user --name scripting
+#python -m ipykernel install --user --name data
