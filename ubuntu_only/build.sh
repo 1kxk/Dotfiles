974da2bab907f6526dca3c2887260625f1327dfb
1kxk <caiow.wk@gmail.com>
2020-04-03 22:37:35 -0300

    .

---
+++ ubuntu_only/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# THEME
+
+git clone https://github.com/daniruiz/flat-remix "$HOME/flat-remix"
+git clone https://github.com/daniruiz/flat-remix-gtk "$HOME/flat-remix-gtk"
+mkdir -p ~/.icons && mkdir -p ~/.themes
+cp -r ~/flat-remix/Flat-Remix* ~/.icons/ && cp -r ~/flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
+
+
