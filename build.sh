0a445cb9f04e513a04e04fba894950bfe4d14769
1kxk <caiow.wk@gmail.com>
2020-04-04 16:15:03 -0300

    additional setup

--- build.sh
+++ build.sh
@@ -14,6 +14,14 @@ else
   CUSTOM_PACKAGES="$(cat .custom-packages)"
 fi
 
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install yarn
+#nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+
 sudo apt update
 sudo apt install $ESSENTIAL_PACKAGES $CUSTOM_PACKAGES -y
 sudo apt upgrade -y
---
+++ system_setup.sh
@@ -0,0 +1,8 @@
+# DEFAULT EDITOR
+select-editor
+sudo update-alternatives --config editor
+
+
+# SET KEYBOARD
+sudo dpkg-reconfigure keyboard-configuration
+
