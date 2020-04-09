78df4c295b5217f9094ec0621560d26192e35bb1
1kxk <caiow.wk@gmail.com>
2020-04-09 01:28:41 -0300

    anaconda_setup

--- stuffs-python.sh
+++ stuffs-python.sh
@@ -3,12 +3,13 @@
 ## Install Anaconda
 echo -e "DOWNLOADING anaconda....\n"
 wget -P "$HOME/Downloads/" https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh 
-#zsh ~/Downloads/Anaconda3-2020.02-Linux-x86_64.sh
-#zsh
+zsh ~/Downloads/Anaconda3-2020.02-Linux-x86_64.sh
 
 ## Config Anaconda
-#conda init zsh
-#conda update -n base -c defaults conda
+~/anaconda/bin/conda init zsh
+conda update -n base -c defaults conda
+conda config --set auto_activate_base false
+zsh
 
 #conda create --name web django flask
 #conda create --name scripting pillow beautifulsoup4
