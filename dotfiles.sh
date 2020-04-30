7aacc69341aa5b7d2d32ea6c98fd1e4ab6a3a088
1kxk <caiow.wk@gmail.com>
2020-04-30 00:38:40 -0300

    .

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,7 +4,7 @@
 # CONFIG DOT FILES #
 ####################
 
-echo -e "Which Dot file you wish to auto setup:\n(1) - VIM\n(2) - VSCODE\n(3) - ZSH\n(4) - SUBLIME\n(5) - EXIT"
+echo -e "Which DotFile?\n(1) - VIM\n(2) - NVIM\n(3) - VSCODE\n(4) - ZSH\n(5) - SUBLIME\n(6) - EXIT"
 read -p "" CHOICE
 
 while [ 1 ]
@@ -14,8 +14,8 @@ do
       rm -rf ~/.vim
       mkdir ~/.vim
       cp -r vim/* ~/.vim
-      read -p "(1) Deschamps / (2) FisaDev " CHOICE
-      if [ $CHOICE -eq 1 ]
+      read -p "(1) Deschamps / (2) FisaDev " OCHOICE
+      if [ $OCHOICE -eq 1 ]
       then
           cp vim/vimrc_deschamps ~/.vimrc
           cp vim/gvimrc_deschamps ~/.gvimrc
@@ -26,9 +26,9 @@ do
       ;;
 
     2)
+        git clone https://github.com/ChristianChiarulli/nvim.git ~/.config/nvim
         curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        git clone https://github.com/ChristianChiarulli/nvim.git ~/.config/nvim 
-
+	;;
 
     3)
       cp -r vscode/settings.json ~/.config/Code/User/settings.json
@@ -54,7 +54,7 @@ do
 
   esac
 
-  echo -e "You want auto setup other DotFile?\n(1) - VIM\n(2) - VSCODE\n(3) - ZSH\n(4) - SUBLIME\n(5) - EXIT"
+  echo -e "You want auto setup other DotFile?\n(1) - VIM\n(2) - NVIM\n(3) - VSCODE\n(4) - ZSH\n(5) - SUBLIME\n(6) - EXIT"
   read -p "" CHOICE
 
 done
