7cda4fa53d5169e5fdd9a1e219963d41b318406e
1kxk <caiow.wk@gmail.com>
2020-06-04 01:26:53 -0300

    kde-only

---
+++ kde_only/animated_wallpaper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir ~/animatedImage
+git clone git@github.com:dark-eye/com.darkeye.animatedImage.git ~/animatedImage
+plasmapkg2 --install ~/animatedImage/com.darkeye.animatedImage
+
