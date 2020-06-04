ce06ee4dbcfea35c4faddfd6895b720123a27cc6
1kxk <caiow.wk@gmail.com>
2020-06-04 02:24:45 -0300

    kde-only

--- kde_only/animated_wallpaper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Gif as wallpaper
-mkdir ~/animatedImage
-git clone git@github.com:dark-eye/com.darkeye.animatedImage.git ~/animatedImage
-plasmapkg2 --install ~/animatedImage/com.darkeye.animatedImage
-
-# Video as wallpaper
-# https://store.kde.org/p/1316299/
