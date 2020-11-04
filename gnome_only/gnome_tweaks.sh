38a42988680f4ec04f9b5d1321a4b3952033a79c
Caio <caiow.wk@gmail.com>
2020-11-04 19:49:26 -0300

    .

--- gnome_only/gnome_tweaks.sh
+++ gnome_only/gnome_tweaks.sh
@@ -8,8 +8,3 @@ git clone https://github.com/daniruiz/flat-remix-gtk "$HOME/flat-remix-gtk"
 mkdir -p ~/.icons && mkdir -p ~/.themes
 cp -r ~/flat-remix/Flat-Remix* ~/.icons/ && cp -r ~/flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
 
-
-# GNOME TERMINAL
-sudo apt-get install dconf-cli
-git clone https://github.com/dracula/gnome-terminal "$HOME/gnome-terminal"
-~/gnome-terminal/install.sh
