ad057df191469a66f81faec12685e1a8cad229f3
Caio <caiow.wk@gmail.com>
2020-11-26 00:12:06 -0300

    .

--- scripts/andoid_studio.sh
+++ scripts/andoid_studio.sh
@@ -8,8 +8,7 @@ sudo add-apt-repository ppa:openjdk-r/ppa
 echo -e "\n\nUPDATE to syncronize the repository....\n"
 sudo apt update
 echo -e "\n\nINSTALLING Java 8 version....\n"
-sudo apt-get install openjdk-8-jdk 
+sudo apt-get install openjdk-8-jdk -y
 echo -e "\n\nINSTALLING graphical libs....\n"
-sudo apt-get install gcc-multilib lib32z1 lib32stdc++6
-
+sudo apt-get install gcc-multilib lib32z1 lib32stdc++6 -y
 
